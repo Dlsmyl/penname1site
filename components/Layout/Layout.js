3dddd503884e1527a9067b487ef626eec2c291a0
Neil Mastroianni <suancho@gmail.com>
2023-01-22 12:21:38 -0500

    fixed skipnav colors

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -15,9 +15,9 @@ const Layout = ({ children, data: { logo, navigationlinks, sociallinks } }) => {
             className="absolute z-50 -top-20 sm:left-1/4 text-2xl text-base-100 inline-block w-full sm:w-1/2 text-center bg-primary-content bg-opacity-30 transform focus:translate-y-20 transition-all duration-500 ease-in-out"
           >
             Skip to main content{' '}
-            <span className="text-gray-800 px-3 py-0 bg-secondary text-base rounded-sm">
+            <span className="text-base-100 px-3 py-0 bg-secondary text-base rounded-sm">
               Return
-              <BsArrowReturnLeft className="w-3 h-3 inline text-gray-800 ml-1" />
+              <BsArrowReturnLeft className="w-3 h-3 inline text-base-100 ml-1" />
             </span>
           </a>
         </li>
