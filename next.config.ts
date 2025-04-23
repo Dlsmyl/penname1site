20add81be88c1f445be45bc4bdcd18cdd25960fa
Neil Mastroianni <nf_mastroianni@hotmail.com>
2025-04-23 17:27:56 -0400

    added remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prismic-io.s3.amazonaws.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
